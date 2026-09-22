73e60dd0a2e87a292278de1691b3467936d57eea
edaUsha <work.pattetiusha@gmail.com>
2026-09-22 17:00:32 +0530

    Update contains_duplicates.py

--- contains_duplicates.py
+++ contains_duplicates.py
@@ -1,4 +1,4 @@
-
+#using for loop with equal logic
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
         for i in range(len(nums)):
@@ -7,4 +7,28 @@ class Solution:
                     return True
                 
         return False
+
+
+#sorting the array
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1]:
+                return True
+                
+        return False
+
+
+#using set function
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        set={}
+        for i in nums:
+            if i in set:
+                return True
+            else:
+                set[i]=1
+        return False
+
 
